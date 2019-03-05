fc112c21b1bc93b0085233e8b534443e9af9849c
DanDanBizarreDevelopment <danielanteby@gmail.com>
2019-03-04 20:22:30 -0500

    GitIgnore ignored me less

---
+++ Assets/Platform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+
+    public float speed;
+    public GameObject endPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+
+        if (endPoint.transform.position.x >= transform.position.x)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/PlatformManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+    public GameObject triggerPoint;
+    public GameObject spawnPoint;
+    public GameObject currObject;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggerPoint.transform.position.x >= currObject.transform.position.x)
+        {
+            currObject = Instantiate(currObject, new Vector3(spawnPoint.transform.position.x, Random.Range(spawnPoint.transform.position.y,
+                triggerPoint.transform.position.y)), spawnPoint.transform.rotation); // New trigger point added when player passes trigger.
+        }
+    }
+}
